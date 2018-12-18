052b847333fb4aaa271fa960dd899904254beb96
Sven <sven@inrain.org>
2018-12-18 19:02:43 +0100

    Add list activity stream and use for adding item

--- client/listling/index.js
+++ client/listling/index.js
@@ -303,8 +303,9 @@ listling.ListPage = class extends micro.Page {
             if (this._data.editMode) {
                 this._form.elements[0].focus();
             } else {
+                const base = `/api/lists/${this._data.lst.id}`;
                 try {
-                    const items = await ui.call("GET", `/api/lists/${this._data.lst.id}/items`);
+                    const items = await ui.call("GET", `${base}/items`);
                     this._items = new micro.bind.Watchable(items);
                 } catch (e) {
                     ui.handleCallError(e);
@@ -323,6 +324,11 @@ listling.ListPage = class extends micro.Page {
                         url: listling.util.makeItemURL(null, item),
                         hash: `map-${item.id.split(":")[1]}`
                     }, item.location)
+                );
+
+                this._activity = await micro.Activity.open(`${base}/activity/stream`);
+                this._activity.events.addEventListener(
+                    "list-create-item", event => this._items.push(event.detail.event.detail.item)
                 );
 
                 if (location.hash === "#presentation") {
@@ -377,9 +383,7 @@ listling.ListPage = class extends micro.Page {
     }
 
     handleEvent(event) {
-        if (event.type === "list-items-create") {
-            this._items.push(event.detail.item);
-        } else if (event.type === "list-items-move") {
+        if (event.type === "list-items-move") {
             let i = this._items.findIndex(item => item.id === event.detail.item.id);
             this._items.splice(i, 1);
             let j = event.detail.to
@@ -471,7 +475,6 @@ listling.ItemElement = class extends HTMLLIElement {
                 } else {
                     this._form.reset();
                     this._form.elements.location.wrapper.value = null;
-                    ui.dispatchEvent(new CustomEvent("list-items-create", {detail: {item}}));
                 }
                 if (this.onedit) {
                     this.onedit(new CustomEvent("edit"));
